ec9a743157484bfa58a641da53b2fec7f45fabaf
Jordan Litsas <jordanlitsas@live.com.au>
2021-10-20 13:11:23 +1000

    real easy rent card

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var port = 8080;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.json())
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 app.use(sqlGenerator);
 app.use(projectRoutes);
 
